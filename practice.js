277d06c93c5ef1fc1dd4022b8ff26778e2cadb4b
David <dave.obat@student.moringashool.com>
2024-01-10 23:30:42 +0300

    conditional expressions

--- practice.js
+++ practice.js
@@ -24,3 +24,51 @@ console.log('aisee')
 const firstName= 'Faith'
 const lastName= 'Mwangi'
 console.log(`My name is ${firstName} ${lastName}`)
+
+//Conditionals
+
+const age= 17
+
+let isAdult
+
+if (age >= 18){
+    isAdult= true
+}else{ //does not have a condition
+    isAdult= false
+}
+
+isAdult //output === false
+
+//ternary expressions
+const color= 'black'
+
+let isColor;
+
+color === 'red' ? (isColor= true) : (isColor= false)
+
+//switch expression
+const colors = 'white'
+let meaning;
+
+switch (colors){
+    case 'red':
+        meaning= "you are bold"
+        break
+    case 'blue':
+        meaning= "you are cool"
+        break
+    case 'yellow':
+        meaning= "you are bubbly"
+        break
+    default:
+        meaning= "you are basic"
+        console.log('you are basic')
+        break
+}
+
+/**
+ * multiline comment
+ * 
+ */
+
+// this is how to start a multiline comment 
