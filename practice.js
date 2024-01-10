74ca92b719667fbda6d95217155253017b0edc20
David <dave.obat@student.moringashool.com>
2024-01-10 22:48:18 +0300

    practiced on variable

--- practice.js
+++ practice.js
@@ -1,1 +1,26 @@
 console.log('HELLO AISEE')
+
+//Data types
+// 1. Numbers
+// 2. Strings
+// 3. Boolean
+// 4. Arrays
+// 5. Objects
+// 6. Undefined
+// 7. Null
+
+// Variable - container
+// let, const, var
+// MUTABILITY AND IMMUTABILITY 
+// Mutability-- can change after creation
+// Immutability-- cannot change after creation
+
+let aisee= 'Faith Waithira'
+aisee= 'Faith Mwangi'
+console.log(aisee) 
+console.log('aisee')
+
+//string interpolation
+const firstName= 'Faith'
+const lastName= 'Mwangi'
+console.log(`My name is ${firstName} ${lastName}`)
